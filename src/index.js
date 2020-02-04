2dce16283980ada0b56e432c4a7b6ed6338554ac
Casey Fayard <fayardcasey@gmail.com>
2020-02-04 14:00:46 -0500

    now successfully fetching data and rendering to dom

--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 const API_ENDPOINT = 'https://swapi.co/api/people/1/';
 
-axios.get(API_ENDPOINT)
-    .then(response => {
-        console.log(response);
-    })
+// axios.get(API_ENDPOINT)
+//     .then(response => {
+//         console.log(response);
+    // })
+
+// fetch(API_ENDPOINT)
+//     .then(r => r.json()
+//     .then(data => {
+//         console.log(data);
+//     }))
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
